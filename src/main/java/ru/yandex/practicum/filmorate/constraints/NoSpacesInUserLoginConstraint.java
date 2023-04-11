679972aed01588ea1cf618eacab0c7a29bf32d7a
noir74y <noir74@yandex.ru>
2023-04-11 23:54:46 +0300

    one more change for style checks

--- src/main/java/ru/yandex/practicum/filmorate/constraints/NoSpacesInUserLoginConstraint.java
+++ src/main/java/ru/yandex/practicum/filmorate/constraints/NoSpacesInUserLoginConstraint.java
@@ -7,8 +7,8 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = NoSpacesInUserLoginValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Constraint (validatedBy = NoSpacesInUserLoginValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface  NoSpacesInUserLoginConstraint {
     String message() default "login can not contain whitespaces";
