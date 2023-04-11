1c44c4e18bf1cd5a17cc265f11115a9fdff3611f
noir74y <noir74@yandex.ru>
2023-04-11 23:51:55 +0300

    one more change for style checks

--- src/main/java/ru/yandex/practicum/filmorate/constraints/FilmReleaseDateConstraint.java
+++ src/main/java/ru/yandex/practicum/filmorate/constraints/FilmReleaseDateConstraint.java
@@ -7,8 +7,8 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint (validatedBy = FilmReleaseDateValidator.class)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Constraint(validatedBy = FilmReleaseDateValidator.class)
+@Target ({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FilmReleaseDateConstraint {
     String message() default "film has wrong release date";
