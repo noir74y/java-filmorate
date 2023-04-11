788c87733c4c5111f1bfe32af8b2c5b1aa681645
noir74y <noir74@yandex.ru>
2023-04-11 23:23:14 +0300

    change for style checks

--- src/main/java/ru/yandex/practicum/filmorate/constraints/FilmDurationConstraint.java
+++ src/main/java/ru/yandex/practicum/filmorate/constraints/FilmDurationConstraint.java
@@ -8,10 +8,12 @@ import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = FilmDurationValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FilmDurationConstraint {
     String message() default "film's duration has to be positive";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
