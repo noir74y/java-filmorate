3b0a6e912e3e408cbf77f5eda0e0241cbb76f619
noir74y <noir74@yandex.ru>
2023-04-13 23:02:46 +0300

    refactor: busines logic of Film is moved to FilmService

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -12,35 +12,26 @@ import java.util.*;
 @RestController
 @RequestMapping("/films")
 @Slf4j
+@RequiredArgsConstructor
 public class FilmController {
+    private final FilmService filmService;
+
     private final Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping()
     public Collection<Film> get() {
-        log.info("get films response {}", films);
-        return films.values();
+        return filmService.get();
     }
 
     @PostMapping
     @ResponseBody
     public Film create(@Valid @RequestBody Film film) {
-        log.info("film create request {}", film);
-        film.setId();
-        films.put(film.getId(),film);
-        log.info("film create response {}", film);
-        return film;
+        return filmService.create(film);
     }
 
     @PutMapping
     @ResponseBody
     public Film update(@Valid @RequestBody Film film) {
-        log.info("film update request {}", film);
-        if (films.containsKey(film.getId())) {
-            films.replace(film.getId(),film);
-            log.info("film update response {}", film);
-            return film;
-        }
-        log.error("no such film {}",film);
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no such film");
+        return filmService.update(film);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,4 +1,43 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
 public class FilmService {
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    public Collection<Film> get() {
+        log.info("get films response {}", films);
+        return films.values();
+    }
+
+    public Film create(Film film) {
+        log.info("film create request {}", film);
+        film.setId();
+        films.put(film.getId(),film);
+        log.info("film create response {}", film);
+        return film;
+    }
+
+    public Film update(Film film) {
+        log.info("film update request {}", film);
+        if (films.containsKey(film.getId())) {
+            films.replace(film.getId(),film);
+            log.info("film update response {}", film);
+            return film;
+        }
+        log.error("no such film {}",film);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no such film");
+    }
 }
