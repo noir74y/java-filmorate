e97b9da1a7ebd68340ea8b398789bfea6580ddc1
noir74y <noir74@yandex.ru>
2023-04-13 22:42:53 +0300

    business logic of Users is moved to UserService

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -12,37 +12,24 @@ import java.util.*;
 @RestController
 @RequestMapping("/users")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
+    private final UserService userService;
 
     @GetMapping()
     public Collection<User> get() {
-        log.info("get users response {}", users);
-        return users.values();
+        return userService.get();
     }
 
     @PostMapping
     @ResponseBody
     public User create(@Valid @RequestBody User user) {
-        log.info("user create request {}", user);
-        user.setId();
-        user.setName(user.getName() == null || user.getName().isBlank() ? user.getLogin() : user.getName());
-        users.put(user.getId(), user);
-        log.info("user create response {}", user);
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     @ResponseBody
     public User update(@Valid @RequestBody User user) {
-        log.info("user update request {}", user);
-        if (users.containsKey(user.getId())) {
-            user.setName(user.getName() == null || user.getName().isBlank() ? user.getLogin() : user.getName());
-            users.replace(user.getId(), user);
-            log.info("user update response {}", user);
-            return user;
-        }
-        log.error("no such user {}",user);
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no such user");
+        return userService.update(user);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,43 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class UserService {
+    private final Map<Integer, User> users = new HashMap<>();
+
+    public Collection<User> get() {
+        log.info("get users response {}", users);
+        return users.values();
+    }
+
+    public User create(User user) {
+        log.info("user create request {}", user);
+        user.setId();
+        user.setName(user.getName() == null || user.getName().isBlank() ? user.getLogin() : user.getName());
+        users.put(user.getId(), user);
+        log.info("user create response {}", user);
+        return user;
+    }
+
+    public User update(User user) {
+        log.info("user update request {}", user);
+        if (users.containsKey(user.getId())) {
+            user.setName(user.getName() == null || user.getName().isBlank() ? user.getLogin() : user.getName());
+            users.replace(user.getId(), user);
+            log.info("user update response {}", user);
+            return user;
+        }
+        log.error("no such user {}",user);
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "no such user");
+    }
+}
