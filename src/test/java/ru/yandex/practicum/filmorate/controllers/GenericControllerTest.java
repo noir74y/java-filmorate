b66f2de25c872fdbf0a840854647cae7b2f751df
noir74y <noir74@yandex.ru>
2023-04-15 15:18:57 +0300

    ref: mock are delete (there are not needed yet)

--- src/test/java/ru/yandex/practicum/filmorate/controllers/GenericControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/GenericControllerTest.java
@@ -1,11 +1,7 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.model.GenericModel;
 
 import javax.validation.ConstraintViolation;
@@ -14,12 +10,7 @@ import javax.validation.Validator;
 import java.util.Set;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 public class GenericControllerTest {
-    @Autowired
-    protected MockMvc mockMvc;
-    @Autowired
-    protected ObjectMapper objectMapper;
     protected static Validator validator;
     protected Set<ConstraintViolation<GenericModel>> violations;
 
