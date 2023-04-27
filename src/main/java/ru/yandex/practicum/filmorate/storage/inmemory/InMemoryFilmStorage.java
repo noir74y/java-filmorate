5b5345b1095007eddb29a39d10062e348a857005
noir74y <noir74@yandex.ru>
2023-04-27 23:48:05 +0300

    feat: добавить реализацию интерфейсов

--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
@@ -1,25 +1,45 @@
 package ru.yandex.practicum.filmorate.storage.inmemory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.storage.interfaces.FilmStorage;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-@Component
+@Service
+@Slf4j
 public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+
     @Override
     public Collection<Film> get() {
-        return null;
+        log.info("get films response {}", films);
+        return films.values();
     }
 
     @Override
     public Film create(Film film) {
-        return null;
+        log.info("film create request {}", film);
+        film.setId();
+        films.put(film.getId(), film);
+        log.info("film create response {}", film);
+        return film;
     }
 
     @Override
     public Film update(Film film) {
-        return null;
+        log.info("film update request {}", film);
+        if (films.containsKey(film.getId())) {
+            films.replace(film.getId(), film);
+            log.info("film update response {}", film);
+            return film;
+        }
+        log.error("no such film {}", film);
+        throw new NotFoundException("no such user");
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
@@ -1,25 +1,52 @@
 package ru.yandex.practicum.filmorate.storage.inmemory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-@Component
+@Service
+@Slf4j
 public class InMemoryUserStorage implements UserStorage {
+
+    private final Map<Integer, User> users = new HashMap<>();
+
     @Override
     public Collection<User> get() {
-        return null;
+        log.info("get users response {}", users);
+        return users.values();
     }
 
     @Override
     public User create(User user) {
-        return null;
+        log.info("user create request {}", user);
+        user.setId();
+        setUserName(user);
+        users.put(user.getId(), user);
+        log.info("user create response {}", user);
+        return user;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        log.info("user update request {}", user);
+        if (users.containsKey(user.getId())) {
+            setUserName(user);
+            users.replace(user.getId(), user);
+            log.info("user update response {}", user);
+            return user;
+        }
+        log.error("no such user {}", user);
+        throw new NotFoundException("no such user");
+    }
+
+    private void setUserName(User user) {
+        user.setName(user.getName() == null || user.getName().isBlank() ? user.getLogin() : user.getName());
     }
 }
