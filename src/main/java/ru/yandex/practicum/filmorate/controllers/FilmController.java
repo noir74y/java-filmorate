d4a3c23eb241c9e647976148767bfdd590161169
noir74y <noir74@yandex.ru>
2023-04-27 23:53:19 +0300

    feat: переделать контроллеры на использование новых имплементаций UserStorage и FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.interfaces.FilmStorage;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -14,24 +14,24 @@ import java.util.*;
 @Slf4j
 @RequiredArgsConstructor
 public class FilmController {
-    private final FilmService filmService;
+    private final FilmStorage filmStorage;
 
     private final Map<Integer, Film> films = new HashMap<>();
 
     @GetMapping()
     public Collection<Film> get() {
-        return filmService.get();
+        return filmStorage.get();
     }
 
     @PostMapping
     @ResponseBody
     public Film create(@Valid @RequestBody Film film) {
-        return filmService.create(film);
+        return filmStorage.create(film);
     }
 
     @PutMapping
     @ResponseBody
     public Film update(@Valid @RequestBody Film film) {
-        return filmService.update(film);
+        return filmStorage.update(film);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -14,22 +14,22 @@ import java.util.*;
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    private final UserStorage userStorage;
 
     @GetMapping()
     public Collection<User> get() {
-        return userService.get();
+        return userStorage.get();
     }
 
     @PostMapping
     @ResponseBody
     public User create(@Valid @RequestBody User user) {
-        return userService.create(user);
+        return userStorage.create(user);
     }
 
     @PutMapping
     @ResponseBody
     public User update(@Valid @RequestBody User user) {
-        return userService.update(user);
+        return userStorage.update(user);
     }
 }
