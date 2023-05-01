eb2cc759235d83a17def1f6cf7d2eb7920df467a
noir74y <noir74@yandex.ru>
2023-05-01 19:40:41 +0300

    feature: сделать логику обработки exception

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ExceptionController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ExceptionController.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.AppException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -30,33 +28,4 @@ public class ExceptionController {
                 .status(appException.getHttpErrorStatus())
                 .body(appException.prepareErrorMessage());
     }
-
-//    @ExceptionHandler(NotFoundException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public ResponseEntity<ErrorMessage> notFoundException(NotFoundException exception) {
-//        return ResponseEntity
-//                .status(HttpStatus.NOT_FOUND)
-//                .body(new ErrorMessage(exception.getMessage()));
-//    }
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public ResponseEntity<ErrorMessage> badRequestException(MethodArgumentNotValidException exception) {
-//        return ResponseEntity
-//                .status(HttpStatus.BAD_REQUEST)
-//                .body(new ErrorMessage(exception.getMessage()));
-//    }
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    ResponseMessage handleException(MethodArgumentNotValidException e) {
-//        SimpleValidateResults results = new SimpleValidateResults();
-//        e.getBindingResult().getAllErrors()
-//                .stream()
-//                .filter(FieldError.class::isInstance)
-//                .map(FieldError.class::cast)
-//                .forEach(fieldError -> results.addResult(fieldError.getField(), fieldError.getDefaultMessage()));
-//        return ResponseMessage.error(400, results.getResults().isEmpty() ? e.getMessage() : results.getResults().get(0).getMessage()).result(results.getResults());
-//    }
-
 }
