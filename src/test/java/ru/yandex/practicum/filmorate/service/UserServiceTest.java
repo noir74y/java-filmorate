7c615a514f5068f2bab28eeb15ab1335713806b2
noir74y <noir74@yandex.ru>
2023-05-04 00:35:04 +0300

    fature: тесты (getUser,getUnknownUser,addFriendship)

--- src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.filmorate.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.model.ErrorMessage;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.inmemory.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -33,50 +39,109 @@ class UserServiceTest {
     private final ApplicationContext applicationContext;
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
+    private static ObjectMapper objectMapper;
     InMemoryUserStorage inMemoryUserStorage;
-    @Autowired
     private UserService userService;
+    private User user1;
+
+    @BeforeAll
+    static void init() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 
     @BeforeEach
     void setUp() {
-//        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) applicationContext.getAutowireCapableBeanFactory();
-//        registry.destroySingleton("UserService");
-//        registry.destroySingleton("InMemoryUserStorage");
-//        //inMemoryUserStorage = applicationContext.getBean(InMemoryUserStorage.class);
-//        //userService = applicationContext.getBean(UserService.class);
-//        registry.registerSingleton("InMemoryUserStorage", new InMemoryUserStorage());
-//        registry.registerSingleton("UserService", new UserService(inMemoryUserStorage));
-    }
+        inMemoryUserStorage = applicationContext.getBean(InMemoryUserStorage.class);
+        userService = applicationContext.getBean(UserService.class);
 
-    @Test
-    void addFriendship() throws Exception {
-
-
-        User user = User.builder().
+        user1 = User.builder().
                 login("dolore").
                 name("Nick Name").
                 email("mail@mail.ru").
                 birthday(LocalDate.of(1946, 8, 20)).
                 build();
 
-        userService.create(user);
+        userService.create(user1);
+    }
 
-//        String responseBody = mockMvc.perform(post("/users").
-//                        content(objectMapper.writeValueAsString(user)).
-//                        contentType(MediaType.APPLICATION_JSON)).
-//                andExpect(status().is(HttpStatus.OK.value())).
-//                andReturn().getResponse().getContentAsString();
-//
-//        System.out.println(responseBody);
-        System.out.println(userService.list().size());
+    @AfterEach
+    void tearDown() {
+        DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) applicationContext.getAutowireCapableBeanFactory();
+        registry.destroySingleton("UserService");
+        registry.destroySingleton("InMemoryUserStorage");
+    }
 
-//                andExpect(jsonPath("$.id").value(1)).
-//                andExpect(jsonPath("$.login").value("dolore")).
-//                andExpect(jsonPath("$.email").value("mail@mail.ru")).
-//                andExpect(jsonPath("$.birthday").value("1946-08-20"));
+    @Test
+    void getUser() throws Exception {
+
+        String responseBody = mockMvc.perform(get("/users/1").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().is(HttpStatus.OK.value())).
+                andReturn().getResponse().getContentAsString();
+
+        User userFromJson = objectMapper.readValue(responseBody, User.class);
+
+        assertEquals(userFromJson.getId(), user1.getId());
+        assertEquals(userFromJson.getEmail(), user1.getEmail());
+        assertEquals(userFromJson.getName(), user1.getName());
+        assertEquals(userFromJson.getBirthday(), user1.getBirthday());
+    }
+
+    @Test
+    void getUnknownUser() throws Exception {
+        String responseBody = mockMvc.perform(get("/users/9999").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().is(HttpStatus.NOT_FOUND.value())).
+                andReturn().getResponse().getContentAsString();
+
+        ErrorMessage errorMessageFromJson = objectMapper.readValue(responseBody, ErrorMessage.class);
+        assertEquals(errorMessageFromJson.getCause(), "no such userId");
+        assertEquals(errorMessageFromJson.getMessage(), "9999");
+    }
+
+    @Test
+    void addFriendship() throws Exception {
+        User friend = User.builder().
+                login("friend").
+                name("friend adipisicing").
+                email("friend@mail.ru").
+                birthday(LocalDate.of(1976, 8, 20)).
+                build();
+
+        userService.create(friend);
+
+        User commonFriend = User.builder().
+                login("common").
+                name("").
+                email("friend@common.ru").
+                birthday(LocalDate.of(2000, 8, 20)).
+                build();
+
+        userService.create(commonFriend);
+
+        String responseBody = mockMvc.perform(get("/users/1/friends/common/2").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().is(HttpStatus.OK.value())).
+                andReturn().getResponse().getContentAsString();
+
+        List<User> friendsList = objectMapper.readValue(responseBody, new TypeReference<>() {
+        });
+
+        assertEquals(0, friendsList.size());
+
+        mockMvc.perform(put("/users/1/friends/2"));
+
+        responseBody = mockMvc.perform(get("/users/1/friends").
+                        contentType(MediaType.APPLICATION_JSON)).
+                andExpect(status().is(HttpStatus.OK.value())).
+                andReturn().getResponse().getContentAsString();
+
+        friendsList = objectMapper.readValue(responseBody, new TypeReference<>() {
+        });
+
+        assertEquals(1, friendsList.size());
+
     }
 
     @Test
@@ -87,16 +152,7 @@ class UserServiceTest {
                 email("mail@mail.ru").
                 birthday(LocalDate.of(1946, 8, 20)).
                 build();
-
         userService.create(user);
-
-//        String responseBody = mockMvc.perform(post("/users").
-//                        content(objectMapper.writeValueAsString(user)).
-//                        contentType(MediaType.APPLICATION_JSON)).
-//                andExpect(status().is(HttpStatus.OK.value())).
-//                andReturn().getResponse().getContentAsString();
-//
-//        System.out.println(responseBody);
         System.out.println(userService.list().size());
     }
 
@@ -108,16 +164,7 @@ class UserServiceTest {
                 email("mail@mail.ru").
                 birthday(LocalDate.of(1946, 8, 20)).
                 build();
-
         userService.create(user);
-
-//        String responseBody = mockMvc.perform(post("/users").
-//                        content(objectMapper.writeValueAsString(user)).
-//                        contentType(MediaType.APPLICATION_JSON)).
-//                andExpect(status().is(HttpStatus.OK.value())).
-//                andReturn().getResponse().getContentAsString();
-//
-//        System.out.println(responseBody);
         System.out.println(userService.list().size());
     }
 
@@ -129,16 +176,7 @@ class UserServiceTest {
                 email("mail@mail.ru").
                 birthday(LocalDate.of(1946, 8, 20)).
                 build();
-
         userService.create(user);
-
-//        String responseBody = mockMvc.perform(post("/users").
-//                        content(objectMapper.writeValueAsString(user)).
-//                        contentType(MediaType.APPLICATION_JSON)).
-//                andExpect(status().is(HttpStatus.OK.value())).
-//                andReturn().getResponse().getContentAsString();
-
-//        System.out.println(responseBody);
         System.out.println(userService.list().size());
     }
 }
