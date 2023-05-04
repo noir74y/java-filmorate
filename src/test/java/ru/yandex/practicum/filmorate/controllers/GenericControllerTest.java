b807268ff35d947734bb12f8f3648252f9fc5e51
noir74y <noir74@yandex.ru>
2023-05-05 01:59:17 +0300

    refactor: remove unused import

--- src/test/java/ru/yandex/practicum/filmorate/controllers/GenericControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/GenericControllerTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.GenericModel;
 
