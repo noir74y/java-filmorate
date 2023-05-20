5439a01b8146b15f2718b88edbff800516afa7ff
noir74y <noir74@yandex.ru>
2023-05-20 20:31:54 +0300

    feat: add H2 intf and impl templates

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/implementations/h2/H2StorageImpl.java
@@ -0,0 +1,134 @@
+package ru.yandex.practicum.filmorate.dao.implementations.h2;
+
+import ru.yandex.practicum.filmorate.dao.interfaces.h2.H2Storage;
+import ru.yandex.practicum.filmorate.model.*;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class H2StorageImpl implements H2Storage {
+    @Override
+    public Collection<Film> getFilms() {
+        return null;
+    }
+
+    @Override
+    public Film getFilm(Integer filmId) {
+        return null;
+    }
+
+    @Override
+    public void createFilm(Integer filmId, Film film) {
+
+    }
+
+    @Override
+    public void createLike(Integer filmId, FilmLikes filmLikes) {
+
+    }
+
+    @Override
+    public void updateFilm(Integer filmId, Film film) {
+
+    }
+
+    @Override
+    public boolean isFilmExists(Integer filmId) {
+        return false;
+    }
+
+    @Override
+    public Collection<FilmLikes> getLikes() {
+        return null;
+    }
+
+    @Override
+    public FilmLikes getFilmLikes(Integer filmId) {
+        return null;
+    }
+
+    @Override
+    public void clearFilms() {
+
+    }
+
+    @Override
+    public void clearLikes() {
+
+    }
+
+    @Override
+    public Collection<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public void createUser(Integer userId, User user) {
+
+    }
+
+    @Override
+    public void createFriends(Integer userId, Set<Integer> friends) {
+
+    }
+
+    @Override
+    public void updateUser(Integer userId, User user) {
+
+    }
+
+    @Override
+    public boolean isUserExists(Integer userId) {
+        return false;
+    }
+
+    @Override
+    public Set<Integer> getFriends(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public void clearUsers() {
+
+    }
+
+    @Override
+    public void clearFriends() {
+
+    }
+
+    @Override
+    public void createFilm(Film film) {
+
+    }
+
+    @Override
+    public void createUser(User user) {
+
+    }
+
+    @Override
+    public Collection<Mpa> getMpa() {
+        return null;
+    }
+
+    @Override
+    public Mpa getMpa(Integer mpaId) {
+        return null;
+    }
+
+    @Override
+    public Collection<Genre> getGenre() {
+        return null;
+    }
+
+    @Override
+    public Mpa getGenre(Integer genreId) {
+        return null;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/dao/interfaces/h2/H2Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/interfaces/h2/H2Storage.java
@@ -2,9 +2,17 @@ package ru.yandex.practicum.filmorate.dao.interfaces.h2;
 
 import ru.yandex.practicum.filmorate.dao.interfaces.generic.GenericStorage;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
 
 public interface H2Storage extends GenericStorage {
     void createFilm(Film film);
     void createUser(User user);
+    Collection<Mpa> getMpa();
+    Mpa getMpa(Integer mpaId);
+    Collection<Genre> getGenre();
+    Mpa getGenre(Integer genreId);
 }
