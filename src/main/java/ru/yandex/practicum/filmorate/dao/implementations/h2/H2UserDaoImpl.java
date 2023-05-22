5e9303d32fad5bf0991288968627efd01ab6c3e2
noir74y <noir74@yandex.ru>
2023-05-23 01:52:43 +0300

    feat: to prepare all methods of H2UserDaoImpl

--- src/main/java/ru/yandex/practicum/filmorate/dao/implementations/h2/H2UserDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/implementations/h2/H2UserDaoImpl.java
@@ -1,15 +1,16 @@
 package ru.yandex.practicum.filmorate.dao.implementations.h2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dao.interfaces.UserDao;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.*;
 
 @Component("H2UserDaoImpl")
 @Slf4j
@@ -23,7 +24,7 @@ public class H2UserDaoImpl extends H2GenericImpl implements UserDao {
                                 row.getInt("id"),
                                 row.getString("email"),
                                 row.getString("login"),
-                                row.getString("mail"),
+                                row.getString("name"),
                                 Objects.requireNonNull(row.getDate("birthday")).toLocalDate()
                         ));
     }
@@ -35,19 +36,36 @@ public class H2UserDaoImpl extends H2GenericImpl implements UserDao {
                 row.getInt("id"),
                 row.getString("email"),
                 row.getString("login"),
-                row.getString("mail"),
+                row.getString("name"),
                 Objects.requireNonNull(row.getDate("birthday")).toLocalDate())
         ) : Optional.empty();
     }
 
     @Override
     public User create(User user) {
-        return null;
+        String sql = "INSERT INTO users(email, login, name, birthday) VALUES (?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sql, new String[]{"id"});
+            stmt.setString(1, user.getEmail());
+            stmt.setString(2, user.getLogin());
+            stmt.setString(3, user.getName());
+            stmt.setDate(4, Date.valueOf(user.getBirthday()));
+            return stmt;
+        }, keyHolder);
+
+        user.setId(Objects.requireNonNull(keyHolder.getKey()).intValue());
+
+        return user;
     }
 
     @Override
     public User update(User user) {
-        return null;
+        jdbcTemplate.update("UPDATE users SET email = ?, login = ?, name = ?, birthdau = ? WHERE id = ?",
+                user.getEmail(), user.getLogin(), user.getName(), Date.valueOf(user.getBirthday()), user.getId());
+        return get(user.getId()).orElse(null);
     }
 
     @Override
@@ -67,7 +85,7 @@ public class H2UserDaoImpl extends H2GenericImpl implements UserDao {
 
     @Override
     public Set<Integer> listUserFriends(Integer userId) {
-        return null;
+        return new HashSet<>(jdbcTemplate.queryForList("SELECT friend_id FROM friends WHERE user_id = ?", Integer.class, userId));
     }
 
     @Override
